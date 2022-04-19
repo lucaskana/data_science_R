9c57e1728a9c7d737d011ec9c7e5f72d3454071d
lucask <luckanadani@gmail.com>
2022-04-19 20:37:40 -0300

    update preparation script

--- utils/prepare_local.R
+++ utils/prepare_local.R
@@ -1,8 +1,12 @@
+
+
 listfiles <- list.files(path="./ESALQ",pattern = "*.zip",full.names=TRUE)
 
 ## Unzip Files
 for(item in listfiles){
-  print(c('Unziping files ->',item))
+  #print(c('Unziping files ->',item))
   destdir <- sub('\\.zip$', '', item)
-  unzip(item, exdir = destdir)
+  destdir <- sub('./ESALQ/', '', destdir)
+  unzip(item, exdir = paste("./scripts_ESALQ",destdir, sep="/"))
+  #print(paste("../script",destdir, sep="/"))
 }
