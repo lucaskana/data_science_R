162b657f6d262b56e5b84bd07095543c3e968e15
lucask <luckanadani@gmail.com>
2022-05-08 21:36:50 -0300

    update - rename files

--- utils/prepare_local.R
+++ utils/prepare_local.R
@@ -1,4 +1,4 @@
-
+library("stringr")
 
 listfiles <- list.files(path="./ESALQ",pattern = "*.zip",full.names=TRUE)
 
@@ -10,3 +10,12 @@ for(item in listfiles){
   unzip(item, exdir = paste("./scripts_ESALQ",destdir, sep="/"))
   #print(paste("../script",destdir, sep="/"))
 }
+
+pattern <-c('\\87','\\xc6','\\xa','\\x82')
+
+listfiles <- list.files(path="./scripts_ESALQ",recursive = TRUE,full.names=TRUE)
+for(f in listfiles){
+  print(f)
+}
+
+file.rename(listfiles,str_replace_all(listfiles,"\\\\", ""))
