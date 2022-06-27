1120a6e2a9f4564c3a2f4fad74111b7f38062a25
Lucas Kanadani <lucas.kanadani@telefonica.com>
2022-06-27 18:02:48 -0300

    scrapting complete

--- src/scripts/TCC_WebScraping_ESALQ.R
+++ src/scripts/TCC_WebScraping_ESALQ.R
@@ -14,11 +14,11 @@ rD <- rsDriver(browser="chrome", chromever="103.0.5060.53", verbose=T)
 remDr <- rD[["client"]]
 remDr$navigate("https://defesas.linka.la/#presentation-anchor")
 pesquisar <- remDr$findElement("class", "rbt")
-input <- pesquisar$findChildElement(using = 'tag name', value = 'input')
+#input <- pesquisar$findChildElement(using = 'tag name', value = 'input')
 ## Criterio de busca
-input$sendKeysToElement(list("NEUROD2", key = "enter"))
 
-option <- remDr$findElement(using = 'xpath',value = "//*[text() = '28/06/2022']")
+
+option <- remDr$findElement(using = 'xpath',value = "//*[text() = '29/06/2022']")
 option$clickElement()
 
 option <- remDr$findElement(using = 'xpath',value = "//*[text() = 'Todos os horários']")
@@ -33,22 +33,49 @@ buttonsearch$clickElement()
 
 ## Result
 result <- remDr$findElement("id", "presentation-anchor")
-resultlist <- result$findChildElements(using = 'xpath', value = "//div[contains(@class, 'mt-4')]")
+
 
 #tcclist <- resultlist[[2]]$findChildElements(using = 'xpath', value = "./div[contains(@class, 'col-12 col-sm-6 col-lg-4 col-xl-3 mb-4 d-flex align-content-between')]")
 
+tcclist <-result$findChildElements(using = 'xpath', value = ".//div[contains(@class, 'col-12 col-sm-6 col-lg-4 col-xl-3 mb-4 d-flex align-content-between')]/div")
 temas <- c()
 # loop version 2
-for (i in 1:length(resultlist)) {
-  tcclist <- resultlist[[i]]$findChildElements(using = 'xpath', value = "./div[contains(@class, 'col-12 col-sm-6 col-lg-4 col-xl-3 mb-4 d-flex align-content-between')]")
-  for (j in 1:length(tcclist)){
-    temas <-  append(temas,tcclist[[j]]$findChildElement(using = 'class name', value = 'col')$getElementText())
-  }
+for (i in 1:length(tcclist)) {
+      temas <-  append(temas,tcclist[[i]]$findChildElement(using = 'tag name', value = 'div')$getElementAttribute('innerHTML'))
 }
 
-print(temas)
+Data <- as.data.frame(do.call(rbind, temas),col.names = c("temas"))  
 
 system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE)
+
+
+
+
+
+
+
+#########################
+#
+# SandBox
+#
+########################
+
+
+#resultlist <- result$findChildElements(using = 'xpath', value = ".//div[contains(@class, 'row mt-4')]")
+
+length(resultlist[[4]]$findChildElements(using = 'xpath', value = ".//div[contains(@class, 'col-12 col-sm-6 col-lg-4 col-xl-3 mb-4 d-flex align-content-between')]"))
+
+tcclist <-result$findChildElements(using = 'xpath', value = ".//div[contains(@class, 'col-12 col-sm-6 col-lg-4 col-xl-3 mb-4 d-flex align-content-between')]/div")
+tcclist[[2]]$findChildElement(using = 'tag name', value = 'div')$getElementAttribute('innerHTML')
+tcclist[[2]]$getElementAttribute('innerHTML')
+tcclist[[1]]$findChildElement(using = 'class name', value = 'col')$getElementText()
+
+tcclist[[1]]$getElementAttribute('innerHTML')
+
+Data <- as.data.frame(do.call(rbind, temas),col.names = c("temas"))  
+print(temas)
+
+
 
 
 #dropcourse <- remDr$findElement("id", "courses")
